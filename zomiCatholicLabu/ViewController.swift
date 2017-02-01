96e7d0d45adffb6beea4ee06affd7d6b97487260
Thang Gin <mggin@thangs-macbook.local>
2017-02-01 12:04:25 -0600

    Initial Commit

---
+++ zomiCatholicLabu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zomiCatholicLabu
+//
+//  Created by Thang Gin on 2/1/17.
+//  Copyright © 2017 Thang Gin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
